4d52497e499b3b6e1fa7a688def37931d6229db6
Lukew23 <lwol078@aucklanduni.ac.nz>
2016-07-23 14:45:42 +1200

    fixed input errors

--- MinistryTest/Assets/FollowPoses.cs
+++ MinistryTest/Assets/FollowPoses.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.IO;
+using System;
+using System.Text;
 
 public class FollowPoses : MonoBehaviour {
 
@@ -19,24 +21,13 @@ public class FollowPoses : MonoBehaviour {
     public string posesFileName;
     private Pose[] poses;
     private int poseIndex;
+    public int numPoses;
     private  Transform[] limbs = new Transform[4];
     // Use this for initialization
     void Start () {
-        if(!File.Exists(posesFileName))
-        {
-            Debug.Log("Trying to open nonexistent Pose");
-            return;
-        }
-        FileStream posesFile = File.Open(posesFileName,FileMode.Open);
-        
-        poses = new Pose[100];
-        poses[0] = new Pose();
-        poses[0].leftArmLower = 20;
-        poses[0].leftArmUpper = 40;
-        poses[0].rightArmLower = 60;
-        poses[0].rightArmUpper = 100;
+        LoadPoses(posesFileName);
         poseIndex = 0;
-
+        numPoses = 0;
         limbs[0] = transform.Find("RightLeg");
         limbs[1] = transform.Find("LeftLeg");
         limbs[2] = transform.Find("RightArm");
@@ -63,8 +54,61 @@ public class FollowPoses : MonoBehaviour {
         limbs[3].Find("Lower").GetComponent<MotorController>().targetAngle = poses[poseIndex].leftArmLower;
     }
 
-    void GoToNextPose()
+    public void GoToNextPose()
     {
         poseIndex++;
+    }
+
+    void LoadPoses(string posesFileName)
+    {
+        poses = new Pose[100];
+        numPoses = 0;
+
+        if (!File.Exists(posesFileName))
+        {
+            Debug.Log("Trying to open nonexistent Pose");
+            return;
+        }
+        try
+        {
+            string line;
+            StreamReader reader = new StreamReader(posesFileName, Encoding.Default);
+            using (reader)
+            {
+                do
+                {
+                    line = reader.ReadLine();
+
+                    if (line != null)
+                    {
+                        string[] angles = line.Split(' ');
+                        float[] fAngles = new float[angles.Length];
+                        for (int i = 0; i < angles.Length; i++)
+                        {
+                            fAngles[i] = float.Parse(angles[i]);
+                        }
+                        Pose pose = new Pose();
+                        pose.leftLegLower = fAngles[0];
+                        pose.leftLegUpper = fAngles[1];
+                        pose.rightLegLower = fAngles[2];
+                        pose.rightLegUpper = fAngles[3];
+                        pose.leftArmLower = fAngles[4];
+                        pose.leftArmUpper = fAngles[5];
+                        pose.rightArmLower = fAngles[6];
+                        pose.rightArmUpper = fAngles[7];
+                        poses[numPoses] = pose;
+                        numPoses++;
+                    }
+                } while (line != null);
+
+                reader.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+
     }
 }
